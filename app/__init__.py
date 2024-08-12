bf8dbc5729026572b02dfd893ff17b314d71033f
Giuseppe <dimontegiuseppe.1@gmail.com>
2024-08-12 20:02:58 +0200

    Setup Flask application with Bootstrap integration and blueprint registration

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ import os
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
-login_manager.login_view = 'login'
+login_manager.login_view = 'main.login'
 login_manager.login_message_category = 'info'
 
 def create_app():
@@ -16,6 +16,7 @@ def create_app():
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
@@ -23,5 +24,4 @@ def create_app():
     from app.routes import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
-    from app import models  
     return app
