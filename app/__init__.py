9e7984dd4894d5e1f350fb3a60ae21e385cb9f32
Giuseppe <dimontegiuseppe.1@gmail.com>
2024-08-13 16:53:30 +0200

    Implement user registration functionality and set up initial database migration

--- app/__init__.py
+++ app/__init__.py
@@ -2,24 +2,24 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
+from flask_migrate import Migrate  # Import Flask-Migrate
 from dotenv import load_dotenv
 import os
 
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
-login_manager.login_view = 'main.login'
-login_manager.login_message_category = 'info'
+migrate = Migrate()  # Initialize Flask-Migrate
 
 def create_app():
     load_dotenv()  # Load environment variables from .env file
     app = Flask(__name__)
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
     app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
-
     db.init_app(app)
     bcrypt.init_app(app)
     login_manager.init_app(app)
+    migrate.init_app(app, db)  
 
     from app.routes import main as main_blueprint
     app.register_blueprint(main_blueprint)
