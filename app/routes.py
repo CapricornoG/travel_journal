4a04667bd54a1f0c68bf285b2f7525884e7d0db1
Giuseppe <dimontegiuseppe.1@gmail.com>
2024-08-14 11:52:14 +0200

    Updated navigation logic for authenticated and unauthenticated users, added journal entry template, and refined home and base templates.

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, flash, request
-from app import db, bcrypt
+from app import db, bcrypt, login_manager
 from app.models import User, JournalEntry
 from app.forms import RegistrationForm, LoginForm, JournalEntryForm
 from flask_login import login_user, current_user, logout_user, login_required
@@ -9,7 +9,8 @@ main = Blueprint('main', __name__)
 
 @main.route('/')
 def home():
-    return render_template('home.html')
+    public_entries = JournalEntry.query.filter_by(is_public=True).all()
+    return render_template('home.html', public_entries=public_entries)
 
 @main.route('/about')
 def about():
@@ -28,7 +29,6 @@ def register():
         flash('Your account has been created! You can now log in', 'success')
         return redirect(url_for('main.login'))
     return render_template('register.html', title='Register', form=form)
-
 
 @main.route('/login', methods=['GET', 'POST'])
 def login():
@@ -76,7 +76,6 @@ def new_journal_entry():
 @login_required
 def journal_entries():
     entries = JournalEntry.query.filter_by(author=current_user).all()
-    print(entries)  # This line can help with debugging
     return render_template('journal_entries.html', journal_entries=entries)
 
 @main.route('/journal/entry/<int:entry_id>')
@@ -89,3 +88,7 @@ def journal_entry(entry_id):
 def public_journal_entries():
     entries = JournalEntry.query.filter_by(is_public=True).all()
     return render_template('journal_entries.html', journal_entries=entries)
+
+@login_manager.unauthorized_handler
+def unauthorized():
+    return render_template('unauthorized.html'), 403
